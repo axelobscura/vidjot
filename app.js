ea674290a1a9c7ae5041b95cf0f6d33da0e06ac0
Axel <axosar@gmail.com>
2018-03-01 18:42:30 -0600

    Middlewares
    Testing a simple middleware…

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
-
 const app = express();
+
+
+app.use(function(req, res, next) {
+    //console.log(Date.now());
+    req.name = 'Axel obscura';
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('INDEX');
+    console.log(req.name);
+});
+
+app.get('/about', (req, res) => {
+    res.send('ABOUT ONE');
+});
 
 const port = 5000;
 
