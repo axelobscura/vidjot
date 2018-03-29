d102845038ebc887c5343bfb8079fdf4216390fa
Axel <axosar@gmail.com>
2018-03-28 19:19:42 -0600

    FLASH MESSAGES
    Displaying flash messages...

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -26,6 +28,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(flash());
+
+app.use(function(req,res,next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.use(function(req, res, next) {
     //console.log(Date.now());
@@ -100,6 +117,7 @@ app.post('/ideas', (req, res) => {
         new Idea(newUser)
             .save()
             .then(idea => {
+                req.flash('success_msg', 'Video Idea Added');
                 res.redirect('/ideas');
             })
     }
@@ -115,6 +133,7 @@ app.put('/ideas/:id', (req, res) => {
 
         idea.save()
             .then(idea => {
+                req.flash('success_msg', 'Video Idea Updated');
                 res.redirect('/ideas');
             })
     })
@@ -123,6 +142,7 @@ app.put('/ideas/:id', (req, res) => {
 app.delete('/ideas/:id', (req, res) => {
    Idea.remove({_id: req.params.id})
     .then(() => {
+        req.flash('success_msg', 'Video Idea Removed');
         res.redirect('/ideas');
     })
 });
